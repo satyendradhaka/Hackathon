7466996ad462159aa348469f54977891b9e1448c
Vatsu919 <vatsunp11@gmail.com>
2020-06-04 15:38:26 +0530

    Added label feature

--- app.js
+++ app.js
@@ -19,13 +19,22 @@ app.use(methodOverride('_method'))
 var todoSchema = new mongoose.Schema({
     title:String,
     status: String,
+    Label:String,
     created: {type: Date, default: Date.now},
-    dueDate: Date
+    dueDate: Date,
+    author:{
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	},
 })
 
 var Todo = mongoose.model('Todo', todoSchema);
 //user schema
 var UserSchema = new mongoose.Schema({
+    name: String,
     username: String,
     password: String
 })
@@ -49,21 +58,42 @@ app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
-
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+});
 
 //routes
+//home page
 app.get('/', function(req, res){
+    if(req.isAuthenticated()){
+        res.redirect('/todo')
+    }else{
+        res.redirect('/login')
+    }
+})
+
+//show todos
+app.get('/todo', isLoggedIn, function(req, res){
     Todo.find({}, function(err, todo){
         if(err){
             console.log(err)
         }else{
-            res.render('home', {todo: todo})
+            res.render('todo', {todo: todo})
         }
     })
 });
-
-app.post('/', function(req, res){
-    Todo.create(req.body.todo, function(err, newTodo){
+//add new todo logic handeler
+app.post('/todo', isLoggedIn, function(req, res){
+    var title= req.body.title;
+    var dueDate = req.body.dueDate
+    var author= {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var label= req.body.Label
+    var newTodo={title: title,Label : label, dueDate: dueDate, author: author}
+    Todo.create(newTodo, function(err, newTodo){
         if(err){
             res.redirect('/')
             console.log(err)
@@ -71,8 +101,8 @@ app.post('/', function(req, res){
         res.redirect('/')
     })
 })
-
-app.get('/archive', function(req, res){
+//archived todos
+app.get('/archive', isLoggedIn, function(req, res){
     Todo.find({'status': 'archive'}, function(err, todo){
         if(err){
             console.log(err)
@@ -81,8 +111,8 @@ app.get('/archive', function(req, res){
         res.render('archive', {todo: todo})
     })
 })
-
-app.put('/:id', function(req, res){
+//make todos archive or active logic handeler
+app.put('/todo/:id', isLoggedIn, function(req, res){
     console.log(req.body)
     console.log(req.params.id)
     Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, updatedTodo){
@@ -95,6 +125,53 @@ app.put('/:id', function(req, res){
     })
 })
 
-app.listen(port, function(){
+//auth routes
+//user registration routes
+app.get('/register', function(req, res){
+    res.render('register')
+})
+//user register handeling
+app.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            res.redirect('/register')
+        }
+        passport.authenticate('local')(req, res, function(){
+            console.log(user.name)
+            res.redirect('/')
+        })
+    })
+})
+
+//show login form
+app.get('/login', function(req, res){
+    res.render('login')
+})
+
+//handling login logic
+app.post('/login', passport.authenticate('local',
+{
+    successRedirect: '/',
+    failureRedirect: '/login'
+}), function(req, res){})
+
+//logout
+app.get('/logout', function(req, res){
+    req.logOut()
+    res.redirect('/')
+})
+
+//middlewares
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    }
+    res.redirect("/login");
+}
+
+
+app.listen(8000, function(){
     console.log('list server is up and running')
 })
